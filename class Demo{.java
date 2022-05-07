b32bb555714dab5f905c6f40c5b5a50a16037596
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-07 12:18:02 +0530

    Add files via upload

---
+++ class Demo{.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        // Load database driver in memory
+        Class.forName("org.postgresql.Driver");
+        System.out.println("Driver loaded");
+
+        Connection conn;
+        conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test1", "postgres", "pravin");
+
+        System.out.println("Coonection done");
+
+        Statement stmt;
+        stmt=conn.createStatement();
+        String sql;
+        sql="insert into student values(356,'testing')";
+        stmt.executeUpdate(sql);
+        System.out.println("record inserted");
+
+        conn.close();
+
+    }
+}
+
